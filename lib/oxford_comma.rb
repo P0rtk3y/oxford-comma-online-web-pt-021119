d579f9ac1b95e811a1e5d9959defb6566c6aab08
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-22 05:06:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 require 'pry'
 def oxford_comma(array)
-  if array.length == 1 
+  if array.length < 2 
     return array[0]
   elsif array.length == 2 
     return array.join(" and ")
-  elsif array.length >= 3
+  elsif array.length > 2
     return array.join(", ").insert(-2, "and ")
   end 
 end
