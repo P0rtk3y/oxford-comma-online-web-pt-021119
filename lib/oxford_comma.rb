95ac73025f95b4fc00a6233fc2e3ee458382d32b
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-22 04:54:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 require 'pry'
 def oxford_comma(array)
   binding.pry
-  if array.length = 1 
+  if array.length == 1 
     return array[0]
-  elsif array.length = 2 
+  elsif array.length == 2 
     return array.join(" and ")
   end 
 end
